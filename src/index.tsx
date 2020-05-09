c71e043da14a70e8c537ffaabd37fecc6a31b049
unknown <kross78080@yandex.ru>
2020-05-10 02:50:43 +0300

    add redux-persist

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,32 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
 import { enableBatching } from 'redux-batched-actions';
 import { Application } from './Componets/Application';
 import reducer from './reducers';
 import './public/locales';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 export const store = configureStore({
-  reducer: enableBatching(reducer),
+  reducer: enableBatching(persistedReducer),
 });
+
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <Application />
+    <PersistGate loading={null} persistor={persistor}>
+      <Application />
+    </PersistGate>
   </Provider>,
   document.querySelector('#content'),
 );
